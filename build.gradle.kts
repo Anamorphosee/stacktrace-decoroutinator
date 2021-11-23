80d285d7d6768b1b86b56fa44106d76c0367b442
berest <berestinsky@gmail.com>
2021-11-23 11:37:02 +0300

    deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.0"
+    `maven-publish`
+    signing
 }
 
 group = "dev.reformator.stacktracedecoroutinator"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -26,4 +28,59 @@ tasks.test {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
     kotlinOptions.freeCompilerArgs += listOf("-Xallow-kotlin-package")
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                name.set("Stacktrace-decoroutinator")
+                description.set("Library for recovering stack trace in exceptions thrown in Kotlin coroutines.")
+                url.set("https://stacktracedecoroutinator.reformator.dev")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Denis Berestinskii")
+                        email.set("berestinsky@gmail.com")
+                        url.set("https://github.com/Anamorphosee")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Anamorphosee/stacktrace-decoroutinator.git")
+                    developerConnection.set("scm:git:ssh://github.com:Anamorphosee/stacktrace-decoroutinator.git")
+                    url.set("http://github.com/Anamorphosee/stacktrace-decoroutinator/tree/master")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releaseRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) {
+                snapshotRepoUrl
+            } else {
+                releaseRepoUrl
+            }
+            credentials {
+                username = properties["sonatype.username"] as String
+                password = properties["sonatype.password"] as String
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["maven"])
 }
